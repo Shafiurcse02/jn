96b7fe3aa2063bc302362acf39c5aff7f278808b
Shafiurcse02 <shafiurcse@gmail.com>
2022-11-09 12:40:44 +0600

    first commit

--- src/test/java/com/jenkins/nactar/SpringJenkinsApplicationTests.java
+++ src/test/java/com/jenkins/nactar/SpringJenkinsApplicationTests.java
@@ -1,16 +1,22 @@
 package com.jenkins.nactar;
 
-import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import static org.junit.Assert.assertEquals;
 
-@RunWith(SpringRunner.class)
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+
 @SpringBootTest
 class SpringJenkinsApplicationTests {
 
+	public static final Logger logger=LoggerFactory.getLogger(SpringJenkinsApplication.class);
 	@Test
 	void contextLoads() {
+		logger.info("RUNNER Clas Shafiur kkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk");
+		assertEquals(true, true);
 	}
 
 }
