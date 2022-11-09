3c182bc63616357cd403918ffad717fa793b522e
Shafiurcse02 <shafiurcse@gmail.com>
2022-11-09 14:10:32 +0600

    First Jenkins

---
+++ src/main/java/com/jenkins/nactar/SpringJenkinsApplication.java
@@ -0,0 +1,18 @@
+package com.jenkins.nactar;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJenkinsApplication {
+	public static Logger logger=LoggerFactory.getLogger(SpringJenkinsApplication.class);
+
+	public static void main(String[] args) {
+		System.out.println("Shafiur");
+		logger.info("NACTAR");
+		SpringApplication.run(SpringJenkinsApplication.class, args);
+	}
+
+}
